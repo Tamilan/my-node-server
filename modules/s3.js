6159e1c6878632de8b948e06a4f551aaf5ad684e
Tamilan <tamlancse089@gmail.com>
2021-03-02 13:42:55 +0530

    second commit

---
+++ modules/s3.js
@@ -0,0 +1,72 @@
+var AWS = require('aws-sdk');
+const fs = require('fs');
+
+
+// var s3  = new AWS.S3({
+//           accessKeyId: 'minioadmin' ,
+//           secretAccessKey: 'minioadmin' ,
+//           endpoint: 'https://192.168.77.32:9001' ,
+//           s3ForcePathStyle: true, // needed with minio?
+//           signatureVersion: 'v4',
+// 		  //s3DisableBodySigning: false
+// });
+
+// process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+class AWSS3 {
+	constructor() {
+		this.s3  = new AWS.S3({
+			accessKeyId: 'minioadmin' ,
+			secretAccessKey: 'minioadmin' ,
+			endpoint: 'https://192.168.77.32:9001' ,
+			s3ForcePathStyle: true, // needed with minio?
+			signatureVersion: 'v4',
+			//s3DisableBodySigning: false
+  		});
+		process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+	}
+
+	add_bucket(param, cb) {
+		var params = {
+			Bucket: param.name
+		};
+		this.s3.createBucket(params, function(err, data) {
+			let response = {
+				"status": "error"
+			};
+			if (err) {
+				console.log(err, err.stack); // an error occurred
+				response['message'] = 'Error in add bucket.';
+			} else {
+				response['status'] = 'success';
+				response['message'] = 'Bucket added.';
+				response['data'] = data;
+			}
+			cb(response);
+		});
+	}
+
+	list_buckets(param, cb) {
+		this.s3.listBuckets(function(err, data) {
+			// if (err) console.log(err, err.stack); // an error occurred
+			// else     console.log(data);           // successful response
+			let response = {
+				"status": "error"
+			};
+			if (err) {
+				console.log(err, err.stack); // an error occurred
+				response['message'] = 'Error in list bucket.';
+			} else {
+				response['status'] = 'success';
+				//response['message'] = 'Bucket added.';
+				response['data'] = data;
+			}
+			cb(response);
+		});
+
+		
+	}
+}
+
+module.exports = {AWSS3};
+//module.exports.AWSS3 = AWSS3;
---
+++ routes/s3.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+
+var {AWSS3} = require('../modules/s3');
+
+const s3 = new AWSS3();
+
+router.get('/add-bucket', function(req, res, next) {
+
+	//let s3 = new AWSS3();
+	//console.log(s3);
+	
+	s3.add_bucket({'name': "tamilan1"}, function(data) {
+		console.log(data);
+		res.send(data);
+	});
+	//res.render('index', { title: 'Express' });
+	//console.log('res');
+	
+});
+
+router.get('/buckets', function(req, res, next) {
+
+	s3.list_buckets({}, function(data) {
+		console.log(data);
+		res.send(data);
+	});
+	
+});
+
+
+router.post('/auth', function(req, res, next) {
+	console.log(req.body);
+	let response = {};
+	if(req.body.params!=undefined) {
+		response.id = 1;
+		response.email = req.body.params.user.email;
+		response.name = "Tamil";
+		response.role = "Admin";
+		response.token = "12345678";
+	} else {
+		response = {
+			"message" : "Invalid request"
+		};
+	}
+	res.send(response);
+});
+
+module.exports = router;
